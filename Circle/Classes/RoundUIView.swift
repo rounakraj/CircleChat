39f6a28c99ce2382347788b7da4d868675b5dada
rounakkumar <krounak@gmail.com>
2018-09-10 11:05:05 +0530

    Added for making Custom Round Views

---
+++ Circle/Classes/RoundUIView.swift
@@ -0,0 +1,33 @@
+//
+//  RoundUIView.swift
+//  Circle
+//
+//  Created by Kumar Rounak on 10/09/18.
+//  Copyright © 2018 Kumar Rounak. All rights reserved.
+//
+
+import Foundation
+
+
+@IBDesignable
+class RoundUIView: UIView {
+    
+    @IBInspectable var borderColor: UIColor = UIColor.white {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 2.0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+}
